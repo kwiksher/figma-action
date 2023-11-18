0b9f422a7dcb2e9c088031782f3ff18b430cd94f
Naoya Yamamoto <yamamoto@kwiksher.com>
2023-11-18 17:51:07 +0900

    Modify to use frames.yml with exportSettings,  data.png.json/data.jpg.json

--- entrypoint.js
+++ entrypoint.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const got = require('got')
 const {ensureDir, writeFile} = require('fs-extra')
 const {join, resolve} = require('path')
@@ -23,6 +24,8 @@ if(!FIGMA_TOKEN) {
   throw Error('Cannot find FIGMA_TOKEN in process!')
 }
 
+console.log("###",FIGMA_FILE_URL)
+
 const client = Figma.Client({
   personalAccessToken: FIGMA_TOKEN
 })
@@ -37,86 +40,124 @@ if (!fileId) {
   }
 }
 
-console.log(`Exporting ${FIGMA_FILE_URL} components`)
-client.file(fileId)
+const yaml = require('js-yaml');
+const fs   = require('fs');
 
-  .then(({ data }) => {
-    console.log('Processing response')
-    const components = {}
+// Get document, or throw exception on error
+try {
+  const doc = yaml.load(fs.readFileSync('./frames.yml', 'utf8'));
+  console.log(doc);
+  for(const target of doc.frames ){
+    exportFrame(target, doc);
+  }
+} catch (e) {
+  console.log(e);
+}
 
-    function check(c) {
-      if (c.type === 'COMPONENT') {
-        const {name, id} = c
-        const {description = '', key} = data.components[c.id]
-        const {width, height} = c.absoluteBoundingBox
-        const filename = `${sanitize(name).toLowerCase()}.${options.format}`;
-
-        components[id] = {
-          name,
-          filename,
-          id,
-          key,
-          file: fileId,
-          description,
-          width,
-          height
+function exportFrame(target, doc){
+  let outputDir = options.outputDir + target;
+  let frame_x = 0;
+  let frame_y = 0;
+  //
+  console.log(`Exporting ${FIGMA_FILE_URL} components`)
+  client.file(fileId)
+    .then(({ data }) => {
+      console.log('Processing response')
+      const components = {}
+      let isTargetFound = false;
+      function check(c) {
+        // if (c.type === 'COMPONENT') {
+          if (c.hasOwnProperty('exportSettings') && c.exportSettings.length>0 ){
+          const {name, id} = c
+          // const {description = '', key} = data.components[c.id]
+          const {width, height} = c.absoluteBoundingBox
+          const filename = `${sanitize(name).toLowerCase()}.${options.format}`;
+          const x = c.absoluteBoundingBox.x - frame_x;
+          const y = c.absoluteBoundingBox.y - frame_y;
+          if (options.format == c.exportSettings[0].format.toLowerCase()){
+            components[id] = {
+              name,
+              filename,
+              id,
+              // key,
+              file: fileId,
+              // description,
+              width,
+              height,
+              x,
+              y
+            }
+          }
+        } else if (c.children) {
+            // console.log(c)
+            if (c.type == "CANVAS" && doc.page==c.name) {
+              // eslint-disable-next-line github/array-foreach
+              c.children.forEach(check)
+            }else if(c.type == "SECTION" && doc.section == c.name ){
+              c.children.forEach(check)
+            }else if (c.type == "FRAME"  && target == c.name){
+              frame_x = c.absoluteBoundingBox.x;
+              frame_y = c.absoluteBoundingBox.y;
+              isTargetFound = true;
+              c.children.forEach(check)
+              isTargetFound = false;
+            }else if (isTargetFound){
+              c.children.forEach(check)
+            }
         }
-      } else if (c.children) {
-        // eslint-disable-next-line github/array-foreach
-        c.children.forEach(check)
       }
-    }
 
-    data.document.children.forEach(check)
-    if (Object.values(components).length === 0) {
-      throw Error('No components found!')
-    }
-    console.log(`${Object.values(components).length} components found in the figma file`)
-    return components
-  })
-  .then(components => {
-    console.log('Getting export urls')
-    return client.fileImages(
-      fileId,
-      {
-        format: options.format,
-        ids: Object.keys(components),
-        scale: options.scale
+      data.document.children.forEach(check)
+      if (Object.values(components).length === 0) {
+        throw Error('No components found! check ' + doc.page + ">" + doc.section + ">" + target)
       }
-    ).then(({data}) => {
-      for(const id of Object.keys(data.images)) {
-        components[id].image = data.images[id]
-      }
+      console.log(`${Object.values(components).length} ${options.format} exports found in the figma file`)
       return components
     })
-  })
-  .then(components => {
-    return ensureDir(join(options.outputDir))
-      .then(() => writeFile(resolve(options.outputDir, 'data.json'), JSON.stringify(components), 'utf8'))
-      .then(() => components)
-  })
-  .then(components => {
-    const contentTypes = {
-      'svg': 'image/svg+xml',
-      'png': 'image/png',
-      'jpg': 'image/jpeg'
-    }
-    return queueTasks(Object.values(components).map(component => () => {
-      return got.get(component.image, {
-        headers: {
-          'Content-Type': contentTypes[options.format]
-        },
-        encoding: (options.format === 'svg' ? 'utf8' : null)
+    .then(components => {
+      console.log('Getting export urls')
+      return client.fileImages(
+        fileId,
+        {
+          format: options.format,
+          ids: Object.keys(components),
+          scale: options.scale
+        }
+      ).then(({data}) => {
+        for(const id of Object.keys(data.images)) {
+          components[id].image = data.images[id]
+        }
+        return components
       })
-      .then(response => {
-        return ensureDir(join(options.outputDir, options.format))
-          .then(() => writeFile(join(options.outputDir, options.format, component.filename), response.body, (options.format === 'svg' ? 'utf8' : 'binary')))
-      })
-    }))
-  })
-  .catch(error => {
-    throw Error(`Error fetching components from Figma: ${error}`)
-  })
+    })
+    .then(components => {
+      return ensureDir(join(outputDir))
+        .then(() => writeFile(resolve(outputDir, `data.${options.format}.json`), JSON.stringify(components), 'utf8'))
+        .then(() => components)
+    })
+    .then(components => {
+      const contentTypes = {
+        'svg': 'image/svg+xml',
+        'png': 'image/png',
+        'jpg': 'image/jpeg'
+      }
+      return queueTasks(Object.values(components).map(component => () => {
+        return got.get(component.image, {
+          headers: {
+            'Content-Type': contentTypes[options.format]
+          },
+          encoding: (options.format === 'svg' ? 'utf8' : null)
+        })
+        .then(response => {
+          return ensureDir(join(outputDir, options.format))
+            .then(() => writeFile(join(outputDir, options.format, component.filename), response.body, (options.format === 'svg' ? 'utf8' : 'binary')))
+        })
+      }))
+    })
+    .catch(error => {
+      throw Error(`Error fetching components from Figma: ${error}`)
+    })
+}
 
 function queueTasks(tasks, options) {
   const queue = new PQueue(Object.assign({concurrency: 3}, options))
